a42af15236d79e3a67521b4b869f2a50931c8e40
738634 <738634@elf8it221377.ACDM.DS.SAIT.CA>
2019-11-04 19:23:07 -0700

    Admin Authentication If a user is not an admin, they can't manage users.

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author 738634
+ */
+public class AdminFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, 
+            FilterChain chain) throws IOException, ServletException 
+    {
+        HttpServletRequest hsr = (HttpServletRequest)request;
+        HttpSession session = hsr.getSession();
+        
+        String user = (String) session.getAttribute("email");
+        
+        if (!user.contains("admin")) {
+            HttpServletResponse hsre = (HttpServletResponse)response;
+            hsre.sendRedirect("home");
+            return;
+        } 
+        
+        chain.doFilter(request, response);
+    }
+
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void destroy() {}
+    
+
+    
+}
